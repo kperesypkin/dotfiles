0b97423eb716ceae53e92cdcc235c0ea310d3847
Kirill Peresypkin <kperesypkin@gmail.com>
2024-01-08 22:10:18 +0300

    Some changes of emacs plugins autoupdates

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -92,6 +92,8 @@
 
 (use-package auto-package-update
   :config
+  (setq auto-package-update-interval 30)
+  (setq auto-package-update-prompt-before-update t)
   (setq auto-package-update-delete-old-versions t)
   (setq auto-package-update-hide-results t)
   (auto-package-update-maybe))
